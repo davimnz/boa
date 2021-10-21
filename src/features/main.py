783563e32efb951d62d21b0391feae328607b744
vasconcelosdavi <davi.muniz41@gmail.com>
2021-10-21 19:53:56 -0300

    add latitude longitude generator

--- src/features/main.py
+++ src/features/main.py
@@ -2,26 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-def generate_uniform_position(min_value, max_value):
+def generate_positions(max_latitude=51.3494, min_latitude=49.5677,
+                       max_longitude=5.8641, min_longitude=2.6622):
     """
-    Generates two dimensional positions for locations in the same grid.
+    Generates latitude and longitude for supply sites and locations.
+
+    max latitude (Belgium): 51.3494
+    min latitude (Belgium): 49.5677
+    max longitude (Belgium: 5.8641
+    min longitude (Belgium): 2.6622
     """
     raw_data = pd.read_csv('data/data.csv', delimiter=';', decimal=',')
-    positions_x = []
-    positions_y = []
-    for row in raw_data.itertuples(index=False):
-        if row[0] == row[2]:
-            position_x = 0.0
-            position_y = 0.0
-        else:
-            position_x = np.random.uniform(min_value, max_value)
-            position_y = np.random.uniform(min_value, max_value)
-        positions_x.append(position_x)
-        positions_y.append(position_y)
-    raw_data['position_x'] = positions_x
-    raw_data['position_y'] = positions_y
+    num_rows = raw_data.shape[0]
+    latitude = np.random.uniform(min_latitude, max_latitude, num_rows)
+    longitude = np.random.uniform(min_longitude, max_longitude, num_rows)
+    raw_data['latitude'] = latitude
+    raw_data['longitude'] = longitude
     raw_data.to_csv('data/new_data.csv', sep=';', decimal=',', index=False)
 
 
 if __name__ == "__main__":
-    generate_uniform_position(0.0, 1000.0)
+    generate_positions()
