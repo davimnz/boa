6fb5321f3901ecb1f4b2cbfe5a929e7615912adf
vasconcelosdavi <davi.muniz41@gmail.com>
2021-10-24 15:00:59 -0300

    Add distance between two points. Fix position generator.

--- src/features/preprocessing.py
+++ src/features/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def clear_inf(raw_data):
+def clear_inf(raw_data) -> None:
     """
     Deletes rows with inf values
     """
@@ -12,7 +12,7 @@ def clear_inf(raw_data):
 
 def generate_positions(raw_data,
                        max_latitude=51.3494, min_latitude=49.5677,
-                       max_longitude=5.8641, min_longitude=2.6622):
+                       max_longitude=5.8641, min_longitude=2.6622) -> None:
     """
     Generates latitude and longitude for supply sites and locations.
 
@@ -21,12 +21,16 @@ def generate_positions(raw_data,
     max longitude (Belgium: 5.8641
     min longitude (Belgium): 2.6622
     """
-    num_rows = raw_data.shape[0]
-    latitude = np.random.uniform(min_latitude, max_latitude, num_rows)
-    longitude = np.random.uniform(min_longitude, max_longitude, num_rows)
-    raw_data['latitude'] = latitude
-    raw_data['longitude'] = longitude
-    raw_data.to_csv('data/new_data.csv', sep=';', decimal=',', index=False)
+    distances = pd.DataFrame()
+    supply_sites = raw_data['Supply Site Code'].unique()
+    locations = raw_data['Location Code'].unique()
+    codes = np.unique(np.concatenate((supply_sites, locations), 0))
+    latitude = np.random.uniform(min_latitude, max_latitude, len(codes))
+    longitude = np.random.uniform(min_longitude, max_longitude, len(codes))
+    distances['code'] = codes
+    distances['latitude'] = latitude
+    distances['longitude'] = longitude
+    distances.to_csv('data/distance.csv', sep=';', decimal=',', index=False)
 
 
 if __name__ == "__main__":
