7f74381c0fbea5e399d69be209bf98ab7f1f1a46
alexandremr01 <alexandremr01@gmail.com>
2021-10-24 14:23:49 -0300

    add exploration

---
+++ src/model/exploration.py
@@ -0,0 +1,32 @@
+from fetch import DataSet
+dataset = DataSet()
+grids = dataset.list_grids()
+
+print('Número de grids: ', len(grids), '\n')
+print('Valores distintos por coluna:')
+print(dataset.data.nunique())
+print('\n')
+
+print('Grids por cenário:')
+df = grids.groupby(by=['Scenario']).count()
+print(df)
+print('\n')
+# Anomalies
+print('Anomalias de dados')
+max_larger_min = dataset.data.query('`MaxDOC (Hl)`<`MinDOC (Hl)`')
+print('Anomalia Max < Min:')
+print(max_larger_min)
+
+# No Hub
+print('Sem Hub:')
+no_hub_count = 0
+for _, g in grids.iterrows():
+    supplier = g['Supply Site Code']
+    sku = g['SKU']
+    scenario = g['Scenario']
+    
+    grid = dataset.select_grid(supplier = supplier, sku = sku)
+    if len(grid.hub) == 0:
+        print(supplier, sku, scenario)
+        no_hub_count += 1
+print(no_hub_count, 'sem hub')
