4f687389ae23be3d4a0cd2504d6c929ff73ce072
vasconcelosdavi <davi.muniz41@gmail.com>
2021-10-27 23:34:44 -0300

    Add exchange map comparison.

--- src/visualization/plot_metric.py
+++ src/visualization/plot_metric.py
@@ -1,10 +1,103 @@
+from math import sin, cos, sqrt, atan2, radians
 import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
 
 
-def plot_stock_balance(data, data_name):
+def distance(latitude_1, longitude_1, latitude_2, longitude_2) -> float:
+    """
+    Evaluates the distance between two points on the Earth surface.
+    """
+    earth_radius = 6373.0  # unit : km
+
+    latitude_1_rad = radians(latitude_1)
+    longitude_1_rad = radians(longitude_1)
+    latitude_2_rad = radians(latitude_2)
+    longitude_2_rad = radians(longitude_2)
+
+    distance_longitude = longitude_2_rad - longitude_1_rad
+    distance_latitude = latitude_2_rad - latitude_1_rad
+
+    a = sin(distance_latitude / 2)**2 + cos(latitude_1_rad) * \
+        cos(latitude_2_rad) * sin(distance_longitude / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return earth_radius*c
+
+
+def evaluate_distance(distances, origin, destiny, supply_site_code) -> float:
+    """
+    Evaluates the distance between origin and destiny
+    """
+    if origin != 'Available' and destiny != 'Available':
+        origin_position = distances.loc[distances['code'] == origin]
+        origin_position = (origin_position['latitude'],
+                           origin_position['longitude'])
+
+        destiny_position = distances.loc[distances['code'] == destiny]
+        destiny_position = (destiny_position['latitude'],
+                            destiny_position['longitude'])
+
+        return distance(origin_position[0], origin_position[1],
+                        destiny_position[0], destiny_position[1])
+    elif origin == 'Available' and destiny != supply_site_code:
+        origin_position = distances.loc[distances['code'] == supply_site_code]
+        origin_position = (origin_position['latitude'],
+                           origin_position['longitude'])
+
+        destiny_position = distances.loc[distances['code'] == destiny]
+        destiny_position = (destiny_position['latitude'],
+                            destiny_position['longitude'])
+
+        return distance(origin_position[0], origin_position[1],
+                        destiny_position[0], destiny_position[1])
+    elif origin == 'Available' and destiny == supply_site_code:
+        return 0
+
+
+def evaluate_exchange_map_cost(exchanges, distances,
+                               supply_site_code, sku_code) -> float:
+    """
+    Evaluates the cost of a given exchange map.
+    """
+    exchange_grid = exchanges[exchanges['Supply Site Code']
+                              == supply_site_code]
+    exchange_grid = exchange_grid[exchange_grid['SKU'] == sku_code]
+
+    cost = 0
+    origin_index = exchange_grid.columns.to_list().index('Origin')
+    destiny_index = exchange_grid.columns.to_list().index('Destiny')
+    amount_index = exchange_grid.columns.to_list().index('Amount')
+    for row in exchange_grid.itertuples():
+        origin = row[origin_index + 1]
+        destiny = row[destiny_index + 1]
+        amount = row[amount_index + 1]
+
+        new_cost = amount * evaluate_distance(distances, origin,
+                                              destiny, supply_site_code)
+        cost = cost + new_cost
+    return cost
+
+
+def plot_exchange_map_comparison(exchanges, distances) -> None:
+    exchanges_cost = []
+
+    supply_sites = exchanges['Supply Site Code'].unique()
+    for supply_site_code in supply_sites:
+        supply_site_df = exchanges[exchanges['Supply Site Code']
+                                   == supply_site_code]
+
+        skus = supply_site_df['SKU'].unique()
+        for sku_code in skus:
+            grid_cost = evaluate_exchange_map_cost(exchanges, distances,
+                                                   supply_site_code, sku_code)
+
+            exchanges_cost.append(grid_cost / 1e6)
+    return exchanges_cost
+
+
+def plot_stock_balance(data, data_name) -> None:
     """
     Plots two histograms of the percentage of balanced stocks in a grid.
     The balance points are max stock and reorder stock.
@@ -54,5 +147,10 @@ def plot_stock_balance(data, data_name):
 
 
 if __name__ == '__main__':
-    unbalanced = pd.read_csv('data/new_data.csv', delimiter=';', decimal=',')
+    unbalanced = pd.read_csv('data/data.csv', delimiter=';', decimal=',')
+    distances = pd.read_csv('data/distance.csv', delimiter=';', decimal=',')
+    exchanges = pd.read_csv('output/exchanges_output.csv',
+                            delimiter=';', decimal=',')
+
     plot_stock_balance(unbalanced, 'unbalanced')
+    plot_exchange_map_comparison(exchanges, distances)
