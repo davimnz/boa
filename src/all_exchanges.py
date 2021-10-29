945db91859f329f62fb0f68ca0edd57941e04407
alexandremr01 <alexandremr01@gmail.com>
2021-10-28 22:58:05 -0300

    add requirements

--- src/all_exchanges.py
+++ src/all_exchanges.py
@@ -1,11 +1,10 @@
 from features.fetch import DataSet
 from features.output import ExchangesOutput
 from model.exchanges import ExchangesSolver
+import numpy as np
 
-dataset = DataSet('output/distribution_output_quadprog.csv')
 
-
-def solve_all(verbose=False):
+def solve_all(dataset, verbose=False, with_redistribution=True, output_file = None):
     exchangesOutput = ExchangesOutput()
     grids = dataset.list_grids()
 
@@ -17,13 +16,23 @@ def solve_all(verbose=False):
         supplier_distances = grid.get_supplier_distances()
         destination_distances = grid.get_destination_distances()
         location_codes = grid.get_all_location_codes()
-        from_supply, exchanges = ExchangesSolver(grid, x_opt_dist, x_opt_dep,
-                                                 x_opt_hub, supplier_distances,
-                                                 destination_distances).solve()
+
+        x_opt = np.concatenate([x_opt_dist, x_opt_dep, x_opt_hub])
+        current_stock = grid.get_total_current_stock()
+        if with_redistribution:
+            from_supply, exchanges = ExchangesSolver(grid, x_opt_dist, x_opt_dep,
+                                                    x_opt_hub, supplier_distances,
+                                                    destination_distances).solve()
+        else:
+            exchanges = np.matrix((0, 0))
+            from_supply = x_opt - current_stock
         exchangesOutput.add_data(supplier, sku, location_codes,
                                  from_supply, exchanges)
 
-    exchangesOutput.print('output/exchanges_output.csv')
+    exchangesOutput.print(output_file)
 
+dataset_with_redistribution = DataSet('output/distribution_output_quadprog.csv')
+dataset_without_redistribution = DataSet('output/no_redistribution.csv')
 
-solve_all()
+solve_all(dataset_without_redistribution, with_redistribution=False, output_file='output/exchanges_output.csv')
+solve_all(dataset_with_redistribution, with_redistribution=True, output_file='output/exchanges_output_no_redistribution.csv')
